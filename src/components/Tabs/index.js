23d30b23ddf3d77337519e257565019182f8a2cd
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-01-26 10:52:02 -0300

    init app nubank

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import Header from '~/components/Header';
-
 import { Container } from './styles';
 
-export default function Main() {
+export default function Tabs() {
   return (
     <Container>
-      <Header/>
+
     </Container>
   )
 }
